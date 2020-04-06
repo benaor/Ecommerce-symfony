a7f9852f741afbe7c8eaf5ba222a317c2751e479
benaor <benjamingirard25@gmail.com>
2020-04-06 14:49:23 +0200

    create Home-Page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'Page d\acceuil',
         ]);
     }
 }
