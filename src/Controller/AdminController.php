dab86c717287cc5a17beda85d7db01755fa0ab53
benaor <benjamingirard25@gmail.com>
2020-04-07 18:07:53 +0200

    Create admin space and secure it

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_tdb")
      */
-    public function index()
+    public function tableauDeBord(ArticleRepository $articleRepo)
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        return $this->render('admin/tdb.html.twig', [
+            'controller_name' => 'Tableau de bord Administrateur',
+            'articles' => $articles = $articleRepo->findAll()
         ]);
     }
+
+    /**
+     * @Route("/admin/login", name="admin_login")
+     */
+    public function adminLogin()
+    {
+        return $this->render('admin/login.html.twig', [
+            'controller_name' => 'Connexion a l\'espace administrateur',
+        ]);
+    }
+
+    /**
+     * @Route("/admin/logout", name="admin_logout")
+     */
+    public function adminLogout()
+    {
+    }
+
+
 }
